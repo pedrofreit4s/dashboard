2cfdb747217b41c9e4130007d81b05364f15c1f9
Pedro Freitas <pedrofreit4s12@gmail.com>
2023-03-13 14:01:21 -0300

    InternationalInsuranceModal

--- src/pages/control/simulations/modals/internationalImsurance/index.tsx
+++ src/pages/control/simulations/modals/internationalImsurance/index.tsx
@@ -7,10 +7,11 @@ import { motion } from "framer-motion";
 import { useSimulations } from "../../hooks/useSimulations";
 import { addDays, format } from "date-fns";
 
-import { CustomSelect } from "../../components/customSelect";
+import { CustomSelect, Item } from "../../components/customSelect";
 
 import { api } from "../../../../../shared/services/api";
 import { useIntarnationalInsuranceForm } from "./hooks/useInternationalInsuranceForm";
+import { useCurrencyAndFees } from "../currency/hooks/useCurrencyAndFees";
 
 interface Props {
   simulationId: string;
@@ -19,6 +20,30 @@ interface Props {
 
 export function InternationalInsuranceModal({ close, simulationId }: Props) {
   const [savingIsLoading, setSavingIsLoading] = useState(false);
+
+  // States
+  const [mercadoria, setMercadoria] = useState<Item>({
+    key: "nao",
+    value: "Não",
+  });
+  const [frete, setFrete] = useState<Item>({
+    key: "nao",
+    value: "Não",
+  });
+  const [tributos, setTributos] = useState<Item>({
+    key: "nao",
+    value: "Não",
+  });
+  const [despesas, setDespesas] = useState<Item>({
+    key: "nao",
+    value: "Não",
+  });
+  const [lucrosEsperado, setLucrosEsperados] = useState<Item>({
+    key: "nao",
+    value: "Não",
+  });
+
+  const { currenciesIsLoading, currenciesToSelect } = useCurrencyAndFees();
 
   const { loadSimulationById, simulation } = useSimulations();
 
@@ -59,7 +84,7 @@ export function InternationalInsuranceModal({ close, simulationId }: Props) {
       className="w-screen h-screen bg-simulation-bg overflow-auto p-32 fixed top-0 bottom-0 left-0 right-0"
     >
       <div className="w-full max-w-[675px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <div className="w-full bg-white shadow-simulation-md border border-black/10 rounded-xl py-7 px-8 relative">
+        <div className="w-full bg-white shadow-simulation-md border border-black/10 rounded-xl py-7 px-8 relative overflow-auto max-h-[600px]">
           <Alert
             emoji={"✍️"}
             title="Aqui, você pode selecionar o seguro internacional"
@@ -89,7 +114,166 @@ export function InternationalInsuranceModal({ close, simulationId }: Props) {
                 ) : null}
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-3 mt-3">
+
+            <div className="w-full mt-3 border border-black/10 py-3 px-4 rounded-sm">
+              <label className="text-simulation-label text-[14px] font-semibold">Opções de cobertura</label>
+              <div className="grid grid-cols-1 mt-2 gap-3">
+                <div className="col-span-1 border border-black/10 p-2">
+                  <CustomSelect
+                    id="Mercadoria"
+                    label="Mercadoria"
+                    items={[
+                      {
+                        key: "sim",
+                        value: "Sim",
+                      },
+                      {
+                        key: "nao",
+                        value: "Não",
+                      },
+                    ]}
+                    defaultValue={mercadoria}
+                    select={async (item) => {
+                      setMercadoria(item);
+                    }}
+                  />
+                  {mercadoria.key === "sim" && (
+                    <div className="col-span-1 mt-2">
+                      <Input label="%" isRequired name="valor" />
+                    </div>
+                  )}
+                </div>
+                <div className="col-span-1 border border-black/10 p-2">
+                  <CustomSelect
+                    id="Frete"
+                    label="Frete"
+                    items={[
+                      {
+                        key: "sim",
+                        value: "Sim",
+                      },
+                      {
+                        key: "nao",
+                        value: "Não",
+                      },
+                    ]}
+                    defaultValue={frete}
+                    select={async (item) => {
+                      setFrete(item);
+                    }}
+                  />
+                  {frete.key === "sim" && (
+                    <div className="col-span-1 mt-2">
+                      <Input label="%" isRequired name="valor" />
+                    </div>
+                  )}
+                </div>
+
+                <div className="col-span-1">
+                  <CustomSelect
+                    id="Tributos"
+                    label="Tributos"
+                    items={[
+                      {
+                        key: "sim",
+                        value: "Sim",
+                      },
+                      {
+                        key: "nao",
+                        value: "Não",
+                      },
+                    ]}
+                    defaultValue={tributos}
+                    select={async (item) => {
+                      setTributos(item);
+                    }}
+                  />
+                  {tributos.key === "sim" && (
+                    <div className="col-span-1 mt-2">
+                      <Input label="%" isRequired name="valor" />
+                    </div>
+                  )}
+                </div>
+
+                <div className="col-span-1 border border-black/10 p-2">
+                  <CustomSelect
+                    id="Despesas"
+                    label="Despesas"
+                    items={[
+                      {
+                        key: "sim",
+                        value: "Sim",
+                      },
+                      {
+                        key: "nao",
+                        value: "Não",
+                      },
+                    ]}
+                    defaultValue={despesas}
+                    select={async (item) => {
+                      setDespesas(item);
+                    }}
+                  />
+                  {despesas.key === "sim" && (
+                    <div className="col-span-1 mt-2">
+                      <Input label="%" isRequired name="valor" />
+                    </div>
+                  )}
+                </div>
+
+                <div className="col-span-1 border border-black/10 p-2">
+                  <CustomSelect
+                    id="Lucros Esperado"
+                    label="Lucros Esperado"
+                    items={[
+                      {
+                        key: "sim",
+                        value: "Sim",
+                      },
+                      {
+                        key: "nao",
+                        value: "Não",
+                      },
+                    ]}
+                    defaultValue={lucrosEsperado}
+                    select={async (item) => {
+                      setLucrosEsperados(item);
+                    }}
+                  />
+                  {lucrosEsperado.key === "sim" && (
+                    <div className="col-span-1 mt-2">
+                      <Input label="%" isRequired name="valor" />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full mt-3 border border-black/10 py-3 px-4 rounded-sm">
+              <label className="text-simulation-label text-[14px] font-semibold">Prêmio</label>
+
+              <div className="grid grid-cols-1 mt-2 gap-3">
+                <div className="col-span-1">
+                  {currenciesToSelect && !currenciesIsLoading ? (
+                    <CustomSelect
+                      id="moeda_mercadorias"
+                      label="Moeda do seguro/prêmio"
+                      isRequired
+                      items={currenciesToSelect}
+                      select={async (item) => {}}
+                    />
+                  ) : null}
+                </div>
+                <div className="col-span-1">
+                  <Input label="Percentual do prêmio" type="number" isRequired name="valor" />
+                </div>
+                <div className="col-span-1">
+                  <Input label="Valor mínimo do prêmio" type="number" isRequired name="valor" />
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="grid grid-cols-2 gap-3 mt-3">
               <div className="col-span-1">
                 <Input
                   label="Valor da moeda"
@@ -108,7 +292,7 @@ export function InternationalInsuranceModal({ close, simulationId }: Props) {
                   value={valorBrl}
                 />
               </div>
-            </div>
+            </div> */}
           </form>
         </div>
 
